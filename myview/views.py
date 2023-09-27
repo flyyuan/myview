ff357c6f698fa30c9ec577684dc3b4f5ad0d68c1
flyyuan <740004544@qq.com>
2023-09-28 00:28:19 +0800

    feat: data_json to templates

--- myview/views.py
+++ myview/views.py
@@ -1,9 +1,17 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render
+
 
 def get_status(request):
     return JsonResponse({"status": True})
 
 
 def index(request):
-    return render(request, 'index.html')
+    data = {
+        'name': 'hhhhhh',
+        'age': 30,
+        'hobbies': ['reading', 'traveling', 'cooking']
+    }
+    return render(request, 'index.html', {"status": True, 'data_json': json.dumps(data)})
